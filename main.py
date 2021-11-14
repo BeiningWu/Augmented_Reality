cda3f4704c1846129346d3029b55ba7f6bc11ce1
贝哥 <1312925385@qq.com>
2021-11-14 20:19:01 +0800

    初步完成ar部分逻辑

--- main.py
+++ main.py
@@ -9,17 +9,40 @@ def findArucoMarkers(img, markerSize=6, totalMarkers=250, draw=True):
     key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
     arucoDict = aruco.Dictionary_get(key)
     arucoParam = aruco.DetectorParameters_create()
-    bbox, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
-    print(ids)
+    bboxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
+    # print(ids)
+    if draw:
+        aruco.drawDetectedMarkers(img, bboxs)
 
+    return [bboxs, ids]
+
+
+def augmentAruco(bbox, id, img, imgAug, drawId=True):
+    tl = bbox[0][0][0], bbox[0][0][1]
+    tr = bbox[0][1][0], bbox[0][1][1]
+    br = bbox[0][2][0], bbox[0][2][1]
+    bl = bbox[0][3][0], bbox[0][3][1]
+
+    h, w, c = imgAug.shape
+    pts1 = np.array([tl, tr, br, bl])
+    pts2 = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
+    matirx, _ = cv2.findHomography(pts2, pts1)
+    imgOut = cv2.warpPerspective(imgAug, matirx, (img.shape[1], img.shape[0]))
+
+    return imgOut
 
 
 def main():
     cap = cv2.VideoCapture(2)
-
+    imgAug = cv2.imread('p1.jpg')
     while True:
         success, img = cap.read()
+        arucoFound = findArucoMarkers(img)
 
+        # Loop through all the markers and augment each one
+        if len(arucoFound[0]) != 0:
+            for bbox, id in zip(arucoFound[0], arucoFound[1]):
+                img = augmentAruco(bbox, id, img, imgAug)
         findArucoMarkers(img)
         cv2.imshow('Image', img)
         cv2.waitKey(1)
