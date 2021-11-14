c4e0aaf43953bff7f115a940465aaf8ffab8e8d2
贝哥 <1312925385@qq.com>
2021-11-14 21:46:23 +0800

    ar逻辑完成max

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ import numpy as np
 import os
 
 
+def loadAugImages(path):
+    myList = os.listdir(path)
+    augDicts = {}
+    for imgPath in myList:
+        key = int(os.path.splitext(imgPath)[0])
+        imgAug = cv2.imread(f'{path}/{imgPath}')
+        augDicts[key] = imgAug
+    return augDicts
+
+
 def findArucoMarkers(img, markerSize=6, totalMarkers=250, draw=True):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
@@ -28,13 +38,17 @@ def augmentAruco(bbox, id, img, imgAug, drawId=True):
     pts2 = np.float32([[0, 0], [w, 0], [w, h], [0, h]])
     matirx, _ = cv2.findHomography(pts2, pts1)
     imgOut = cv2.warpPerspective(imgAug, matirx, (img.shape[1], img.shape[0]))
-
+    cv2.fillConvexPoly(img, pts1.astype(int), (0, 0, 0))
+    imgOut = img + imgOut
+    if drawId:
+        cv2.putText(imgOut, str(id), (np.uint32(tl[0]).item(), np.uint32(tl[1]).item()), cv2.FONT_HERSHEY_PLAIN, 2,
+                    (255, 0, 255), 2)
     return imgOut
 
 
 def main():
     cap = cv2.VideoCapture(2)
-    imgAug = cv2.imread('p1.jpg')
+    augDicts = loadAugImages("Markers")
     while True:
         success, img = cap.read()
         arucoFound = findArucoMarkers(img)
@@ -42,7 +56,8 @@ def main():
         # Loop through all the markers and augment each one
         if len(arucoFound[0]) != 0:
             for bbox, id in zip(arucoFound[0], arucoFound[1]):
-                img = augmentAruco(bbox, id, img, imgAug)
+                if int(id) in augDicts:
+                    img = augmentAruco(bbox, id, img, augDicts[int(id)])
         findArucoMarkers(img)
         cv2.imshow('Image', img)
         cv2.waitKey(1)
